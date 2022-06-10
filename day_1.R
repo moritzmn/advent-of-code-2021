3ec1d8793a79ba90717f3ee59985af1de0c57225
moritzmn <muellernavarra@gmail.com>
2022-06-10 10:54:38 +0200

    formatting

--- day_1.R
+++ day_1.R
@@ -5,19 +5,23 @@
 
 # Part one ----------------------------------------------------------------
 
-## Data --------------------------------------------------------------------
+# # * Data --------------------------------------------------------------------
 
 sonar_data <- read.csv(file = "data/day_one_sonar",  col.names = "depth", header = FALSE)
 head(sonar_data)
 nrow(sonar_data)
 
-## Analyze -----------------------------------------------------------------
+# # * Analyze -----------------------------------------------------------------
 
 diff_depth <- diff(sonar_data$depth)
+
 head(diff_depth)
 length(diff_depth)
+
 sum_inc <- sum(diff_depth > 0)
 sum_inc
+
+# Part twp ----------------------------------------------------------------
 
 shift_sum <- vector(mode = "numeric", length = nrow(sonar_data) - 2)
 for(i in seq_along(shift_sum)) {
