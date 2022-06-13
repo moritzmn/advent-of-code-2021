6520d647d8239b4b760bd785e77163d1556a555d
moritzmn <muellernavarra@gmail.com>
2022-06-13 11:39:25 +0200

    formatting

--- day_9.R
+++ day_9.R
@@ -3,6 +3,37 @@
 #~~~~~~~~~~~~~~
 
 
+
+
+
+# solution ----------------------------------------------------------------
+
+# # * part1 -------------------------------------------------------------------
+
+#functions
+MNeighbors <- function(mat, i = 2L, j = 3L) {
+  all(mat[i, j] < c(mat[i, j - c(-1L, 1L)], mat[i - c(-1L, 1L), j]), na.rm = TRUE)
+}
+vMNeighbors <- Vectorize(FUN = MNeighbors, "j")
+
+raw_data <- readLines(con = "data/day_nine")
+integer_split <- lapply(raw_data, function(x){
+  as.integer(strsplit(x, split = "")[[1]])
+})
+m <- do.call(rbind, integer_split)
+#add NA
+m_t <- cbind(NA, rbind(NA, m,  NA), NA)
+
+#get borders
+rows <- 2L : (nrow(m_t) - 1L)
+cols <- 2L : (ncol(m_t) - 1L)
+
+mask <- lapply(rows, function(y) {
+  vMNeighbors(m_t, i = y, j = cols)
+})
+mask <- do.call(rbind, mask)
+sum(m[mask] + 1)
+
 # test --------------------------------------------------------------------
 
 raw_data <- readLines(con = "data/day_nine_test")
@@ -29,31 +60,3 @@ mask <- lapply(rows, function(y) {
 })
 mask <- do.call(rbind, mask)
 m[mask] + 1
-
-
-# solution ----------------------------------------------------------------
-
-# # * part1 -------------------------------------------------------------------
-
-raw_data <- readLines(con = "data/day_nine")
-integer_split <- lapply(raw_data, function(x){
-  as.integer(strsplit(x, split = "")[[1]])
-})
-m <- do.call(rbind, integer_split)
-#add NA
-m_t <- cbind(NA, rbind(NA, m,  NA), NA)
-#functions
-MNeighbors <- function(mat, i = 2L, j = 3L) {
-  all(mat[i, j] < c(mat[i, j - c(-1L, 1L)], mat[i - c(-1L, 1L), j]), na.rm = TRUE)
-}
-vMNeighbors <- Vectorize(FUN = MNeighbors, "j")
-
-#get borders
-rows <- 2L : (nrow(m_t) - 1L)
-cols <- 2L : (ncol(m_t) - 1L)
-
-mask <- lapply(rows, function(y) {
-  vMNeighbors(m_t, i = y, j = cols)
-})
-mask <- do.call(rbind, mask)
-sum(m[mask] + 1)
